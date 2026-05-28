fde04c2f9f1d719279cd4110913aab3184669bab
mehtadiya <mehtadiya675@gmail.com>
2026-05-28 15:22:42 +0530

    updated css

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -261,7 +261,7 @@ function Dashboard() {
                         <h5 className="fw-bold mb-4">Recent Expenses</h5>
 
                         <div className="table-responsive pe-4" style={{
-                            height: "auto",
+                            height: "260px",
                             width: "100%",
                             overflowY: "auto",
                         }}>
